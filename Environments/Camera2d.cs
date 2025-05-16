03c08f98acbd104ddda82aba20e0775327e9aff5
J5osully <j5osully@gmail.com>
2025-05-15 21:38:06 -0500

    more testing with basic player movement

---
+++ Environments/Camera2d.cs
@@ -0,0 +1,27 @@
+using Godot;
+using System;
+
+public partial class Camera2d : Camera2D
+{
+	public override void _Ready()
+	{
+		SetProcess(true); // Optional in Godot 4, but okay to keep
+	}
+
+	public override void _Process(double delta)
+	{
+		float AMOUNT = 5;
+
+		if (Input.IsKeyPressed(Key.W))
+			Position += new Vector2(0, -AMOUNT);
+
+		if (Input.IsKeyPressed(Key.A))
+			Position += new Vector2(-AMOUNT, 0);
+
+		if (Input.IsKeyPressed(Key.S))
+			Position += new Vector2(0, AMOUNT);
+
+		if (Input.IsKeyPressed(Key.D))
+			Position += new Vector2(AMOUNT, 0);
+	}
+}
